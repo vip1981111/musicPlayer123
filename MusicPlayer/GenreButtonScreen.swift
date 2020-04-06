353b0385dcc581137d9589c94c28dfe45dbde56a
mohammed <vip1981.1@gmail.com>
2020-04-06 23:32:03 +0300

    initial

--- MusicPlayer/GenreButtonScreen.swift
+++ MusicPlayer/GenreButtonScreen.swift
@@ -6,4 +6,52 @@
 //  Copyright © 1441 Mohammed Abdullah. All rights reserved.
 //
 
-import Foundation
+import UIKit
+import MediaPlayer
+
+class GenreButtonScreen : UIViewController {
+    
+    var musicPlayer = MPMusicPlayerController.applicationMusicPlayer
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func genreButtonTapped(_ sender: UIButton) {
+        MPMediaLibrary.requestAuthorization { (status) in
+            if status == .authorized {
+                DispatchQueue.main.async { // Correct
+                   self.playeGenre(genre: sender.currentTitle!)
+                }
+            
+               
+            }
+        }
+       
+    }
+    
+    
+    @IBAction func stopButtonTapped(_ sender: UIButton) {
+        musicPlayer.stop()
+    }
+    
+    
+    @IBAction func nextButtonTapped(_ sender: UIButton) {
+        musicPlayer.skipToNextItem()
+    }
+    
+    func playeGenre(genre : String) {
+        
+        musicPlayer.stop()
+        
+        let query = MPMediaQuery()
+        let predicate = MPMediaPropertyPredicate(value: genre, forProperty: MPMediaItemPropertyGenre)
+        
+        query.addFilterPredicate(predicate)
+        
+        musicPlayer.setQueue(with: query)
+        musicPlayer.shuffleMode = .songs
+        musicPlayer.play()
+    }
+    
+}
